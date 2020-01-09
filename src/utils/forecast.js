a8ccbd703b3dd8c3767e71ac6d74ba7c005d2eac
bharath <bharathviru333@gmail.com>
2020-01-09 06:54:06 +0530

    changes to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
                 else if(body.error){
                     callback("unable to find location. Try again.", undefined)
                 } else{
-                    callback(undefined, body.daily.data[0].summary + "it is currently " + body.currently.temperature + "degrees out. there is " + body.currently.precipProbability + "% of rain"
+                    callback(undefined, body.daily.data[0].icon + "." + body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. There is " + body.currently.precipProbability + "% of rain. High Temperature: " + body.daily.data[0].apparentTemperatureHigh + " degrees. Low Temperature: " + body.daily.data[0].apparentTemperatureLow + "degrees."
                         
                     )
                 }
